896edb7451ba59599a5f11f5c4ce5603a4ab01bb
Shuma Yoshioka <s64.stdio@gmail.com>
2015-02-26 22:11:53 +0900

    Update

--- web/wp-cli.sh
+++ web/wp-cli.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+update-alternatives --set php $(which php5)
 pushd /opt/wordpress
 
 wp core download --allow-root --locale=ja --path="/opt/wordpress"
 wp core config --allow-root --dbname='wordpress' --dbuser='root' --dbhost='mysql:3306' --locale=ja
 
 popd
+update-alternatives --set php $(which hhvm)
