82f182e0d4de0a3b5f4c44979a2959a08c228c29
Shuma Yoshioka <s64.stdio@gmail.com>
2015-02-28 09:19:28 +0900

    Update

--- web/wp-cli.sh
+++ web/wp-cli.sh
@@ -2,14 +2,8 @@
 update-alternatives --set php $(which php5)
 pushd /opt/wordpress
 
-cat /etc/hosts
-sleep 10
-
 wp core download --allow-root --locale=ja --path="/opt/wordpress"
-wp core config --allow-root --dbname='wordpress' --dbuser='root' --dbhost='webdb:3306' --locale=ja
-
-cat /opt/wordpress/wp-config.php
-sleep 10
+wp core config --allow-root --dbname='wordpress' --dbuser='root' --dbhost='mysql:3306' --locale=ja
 
 #define('DB_HOST', getenv('MYSQL_PORT_3306_TCP_ADDR').':'.getenv('MYSQL_PORT_3306_TCP_PORT'));
 
