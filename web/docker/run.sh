e51382796ec94b4c6aa3a35e793be8e61fcb34cb
Shuma Yoshioka <s64.stdio@gmail.com>
2015-02-28 10:21:42 +0900

    Update web/docker/run.sh

--- web/docker/run.sh
+++ web/docker/run.sh
@@ -5,6 +5,10 @@ if ! $(wp core is-installed --allow-root); then
   source /opt/wp-cli.sh
 fi
 
+touch /opt/wp_debug.log
+touch /usr/local/nginx/logs/error.log
+touch /usr/local/nginx/logs/access.log
+
 supervisord
 
 popd
