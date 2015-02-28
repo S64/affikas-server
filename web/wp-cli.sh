9684182e81c4e0ffd9cf4b8661bad3a29bedfec8
Shuma Yoshioka <s64.stdio@gmail.com>
2015-02-28 10:05:51 +0900

    Update web/wp-cli.sh

--- web/wp-cli.sh
+++ web/wp-cli.sh
@@ -4,6 +4,11 @@ pushd /opt/wordpress
 
 wp core download --allow-root --locale=ja --path="/opt/wordpress"
 wp core config --allow-root --dbname='wordpress' --dbuser='root' --dbhost='mysql:3306' --locale=ja
+wp core config --locale=ja --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', false);
+define( 'WP_DEBUG_LOG', true );
+PHP
 
 #define('DB_HOST', getenv('MYSQL_PORT_3306_TCP_ADDR').':'.getenv('MYSQL_PORT_3306_TCP_PORT'));
 
