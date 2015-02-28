f700c65c92b9f2ce29eea679521a25642f6acc4e
Shuma Yoshioka <s64.stdio@gmail.com>
2015-02-28 10:10:47 +0900

    Update

--- web/wp-cli.sh
+++ web/wp-cli.sh
@@ -8,6 +8,8 @@ wp core config --locale=ja --extra-php <<PHP
 define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_DISPLAY', false);
 define( 'WP_DEBUG_LOG', true );
+ini_set( 'log_errors', 1 );
+ini_set( 'error_log', '/opt/wp_debug.log' );
 PHP
 
 #define('DB_HOST', getenv('MYSQL_PORT_3306_TCP_ADDR').':'.getenv('MYSQL_PORT_3306_TCP_PORT'));
