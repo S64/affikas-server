a7ce7353fa49b933974b81344b9bddf8a67266d6
Shuma Yoshioka <s64.stdio@gmail.com>
2015-03-01 21:02:58 +0900

    Update

--- web/docker/run.sh
+++ web/docker/run.sh
@@ -2,7 +2,10 @@
 pushd /opt/wordpress
 
 if ! $(wp core is-installed --allow-root); then
-  source /opt/wp-cli.sh
+  source /opt/wp-cli_install.sh
+fi
+if true ; then
+  source /opt/wp-cli_check.sh
 fi
 
 #supervisord
