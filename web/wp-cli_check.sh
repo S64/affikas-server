00f42706f80add306ddb5529d66da1e307adbf0a
Shuma Yoshioka <s64.stdio@gmail.com>
2015-03-01 21:06:53 +0900

    Update

---
+++ web/wp-cli_check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+update-alternatives --set php $(which php5)
+pushd /opt/wordpress
+
+wp plugin install --allow-root wp-multibyte-patch --activate
+wp plugin install --allow-root jetpack --activate
+wp plugin install --allow-root google-sitemap-generator --activate
+wp plugin install --allow-root pushpress --activate
+wp plugin install --allow-root akismet
+wp plugin install --allow-root twitter
+wp plugin install --allow-root facebook
+wp plugin install --allow-root slack
+wp plugin install --allow-root shortcoder
+wp plugin install --allow-root video-thumbnails
+wp plugin install --allow-root backwpup
+wp plugin install --allow-root broken-link-checker
+wp plugin install --allow-root wp-basic-auth
+wp plugin install --allow-root wordfence
+#wp plugin install --allow-root crazy-bone --activate
+
+wp theme install --allow-root twentyten
+wp theme install --allow-root twentyeleven
+wp theme install --allow-root twentytwelve
+wp theme install --allow-root twentythirteen
+wp theme install --allow-root twentyfourteen
+wp theme install --allow-root twentyfifteen
+wp theme install --allow-root wp-jurist
+
+#wp theme activate --allow-root twentyfifteen
+
+wp db optimize --allow-root
+
+popd
+update-alternatives --set php $(which hhvm)
--- web/wp-cli_install.sh
+++ web/wp-cli_install.sh
@@ -13,35 +13,5 @@ PHP
 
 wp core install --allow-root --url='157.7.239.53' --title='WordPress on Affikas' --admin_user='root' --admin_password='password' --admin_email='example@example.com'
 
-wp plugin install --allow-root wp-multibyte-patch --activate
-wp plugin install --allow-root jetpack --activate
-wp plugin install --allow-root google-sitemap-generator --activate
-wp plugin install --allow-root pushpress --activate
-wp plugin install --allow-root akismet
-wp plugin install --allow-root twitter
-wp plugin install --allow-root facebook
-wp plugin install --allow-root slack
-wp plugin install --allow-root shortcoder
-wp plugin install --allow-root video-thumbnails
-wp plugin install --allow-root backwpup
-wp plugin install --allow-root broken-link-checker
-wp plugin install --allow-root wp-basic-auth
-wp plugin install --allow-root wordfence
-#wp plugin install --allow-root crazy-bone --activate
-
-wp theme install --allow-root twentyten
-wp theme install --allow-root twentyeleven
-wp theme install --allow-root twentytwelve
-wp theme install --allow-root twentythirteen
-wp theme install --allow-root twentyfourteen
-wp theme install --allow-root twentyfifteen
-wp theme install --allow-root wp-jurist
-
-wp theme activate --allow-root twentyfifteen
-
-wp db optimize --allow-root
-
-#define('DB_HOST', getenv('MYSQL_PORT_3306_TCP_ADDR').':'.getenv('MYSQL_PORT_3306_TCP_PORT'));
-
 popd
 update-alternatives --set php $(which hhvm)
