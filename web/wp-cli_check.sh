0d0fb16ecccdb4ee0e3fe73b35fd16ae9b199777
Shuma Yoshioka <s64.stdio@gmail.com>
2015-03-04 19:34:51 +0900

    Update

--- web/wp-cli_check.sh
+++ web/wp-cli_check.sh
@@ -17,6 +17,10 @@ wp plugin install --allow-root broken-link-checker
 wp plugin install --allow-root wp-basic-auth
 wp plugin install --allow-root wordfence
 #wp plugin install --allow-root crazy-bone --activate
+wp plugin install --allow-root wpmandrill --activate
+#wp plugin install --allow-root revisr
+#wp plugin install --allow-root gitium
+#wp plugin install --allow-root wordless
 
 wp theme install --allow-root twentyten
 wp theme install --allow-root twentyeleven
@@ -24,7 +28,10 @@ wp theme install --allow-root twentytwelve
 wp theme install --allow-root twentythirteen
 wp theme install --allow-root twentyfourteen
 wp theme install --allow-root twentyfifteen
+
 wp theme install --allow-root wp-jurist
+wp theme install --allow-root hueman
+wp theme install --allow-root flower
 
 #wp theme activate --allow-root twentyfifteen
 
--- web/wp-cli_install.sh
+++ web/wp-cli_install.sh
@@ -13,5 +13,8 @@ PHP
 
 wp core install --allow-root --url='157.7.239.53' --title='WordPress on Affikas' --admin_user='root' --admin_password='password' --admin_email='example@example.com'
 
+cp /opt/composer.json.copyme /opt/wordpress/composer.json
+composer install
+
 popd
 update-alternatives --set php $(which hhvm)
